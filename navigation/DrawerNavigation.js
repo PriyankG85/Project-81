f64f43a95c5cd2d35833502c22d5bf2143d5e8b8
PriyankG85 <84387001+PriyankG85@users.noreply.github.com>
2022-03-22 15:04:52 +0530

    Add files via upload

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -1,14 +1,19 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import React from "react";
+import Logout from "../screens/Logout";
 import Profile from "../screens/Profile";
 import StackNavigator from "./StackNavigator";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigation = () => (
-  <Drawer.Navigator screenOptions={{ headerShown: false }}>
+  <Drawer.Navigator
+    initialRouteName={"Profile"}
+    screenOptions={{ headerShown: false }}
+  >
     <Drawer.Screen name="Home" component={StackNavigator} />
     <Drawer.Screen name="Profile" component={Profile} />
+    <Drawer.Screen name="Logout" component={Logout} />
   </Drawer.Navigator>
 );
 
